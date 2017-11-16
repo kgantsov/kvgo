f3da590ab8c8d132b399deb8286ab44577bf1db9
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-11-16 22:27:53 +0100

    Basic implementation of key value store

---
+++ kv/kv.go
@@ -0,0 +1,174 @@
+package kv
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Index struct {
+	Offset int64
+}
+
+type KV struct {
+	Offset    int64
+	Indexes   []map[string]Index
+	MemTable  map[string]string
+	DbPath    string
+	indexPath string
+}
+
+func NewKV(dbPath, indexPath string) *KV {
+	kv := new(KV)
+	kv.DbPath = dbPath
+	kv.indexPath = indexPath
+
+	err := kv.LoadIndexes()
+	if err != nil {
+		fmt.Println("??????????", err)
+	}
+	kv.MemTable = make(map[string]string)
+	return kv
+}
+
+func (kv *KV) Set(key, value string) {
+	kv.MemTable[key] = value
+}
+
+func (kv *KV) Get(key string) (string, bool) {
+	val, ok := kv.MemTable[key]
+	if ok {
+		fmt.Printf("Key: %s found in memory\n", key)
+		return val, ok
+	}
+
+	f, err := os.Open(kv.DbPath)
+	if err != nil {
+		return "", false
+	}
+
+	defer f.Close()
+
+	for i := len(kv.Indexes) - 1; i >= 0; i-- {
+		indexVal, ok := kv.Indexes[i][key]
+
+		if !ok {
+			continue
+		}
+
+		f.Seek(int64(indexVal.Offset), 0)
+
+		data := make([]byte, 16)
+		_, err := f.Read(data)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		keyLength := binary.BigEndian.Uint64(data[:8])
+		valLength := binary.BigEndian.Uint64(data[8:])
+
+		data = make([]byte, keyLength+valLength)
+		f.Seek(int64(indexVal.Offset+16), 0)
+		_, err = f.Read(data)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		fmt.Printf("Key: %s found on disc. Value: '%s'\n", key, string(data[keyLength:]))
+
+		return string(data[keyLength:]), true
+	}
+	return "", false
+}
+
+func (kv *KV) Delete(key string) {
+	delete(kv.MemTable, key)
+}
+
+func (kv *KV) Flush() {
+	fmt.Printf("!!! Indexes: %v Offset: %d\n", kv.Indexes, kv.Offset)
+
+	if len(kv.MemTable) == 0 {
+		return
+	}
+
+	f, err := os.OpenFile(kv.DbPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	st, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	kv.Offset = st.Size()
+
+	index := make(map[string]Index)
+
+	for k, v := range kv.MemTable {
+		index[k] = Index{kv.Offset}
+		buf := bytes.NewBuffer([]byte{})
+
+		kv.Offset += 16
+		if err = binary.Write(buf, binary.BigEndian, int64(len([]byte(k)))); err != nil {
+			return
+		}
+		if err = binary.Write(buf, binary.BigEndian, int64(len([]byte(v)))); err != nil {
+			return
+		}
+
+		if _, err = buf.Write([]byte(k)); err != nil {
+			return
+		}
+		kv.Offset += int64(len(k))
+
+		if _, err = buf.Write([]byte(v)); err != nil {
+			return
+		}
+		kv.Offset += int64(len(v))
+
+		if _, err := f.Write(buf.Bytes()); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	kv.Indexes = append(kv.Indexes, index)
+	fmt.Printf("@@@ Indexes: %v Offset: %d\n", kv.Indexes, kv.Offset)
+	kv.SaveIndexes()
+
+	f.Close()
+}
+
+func (kv *KV) SaveIndexes() error {
+	file, err := os.OpenFile(kv.indexPath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(kv.Indexes)
+	}
+	file.Close()
+	return err
+}
+
+func (kv *KV) LoadIndexes() error {
+	file, err := os.Open(kv.indexPath)
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(&kv.Indexes)
+	} else {
+
+	}
+	file.Close()
+	return err
+}
+
+func (kv *KV) Close() {
+	kv.Flush()
+}
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kgantsov/kvgo/kv"
+)
+
+const dbPath = "./data.db"
+const indexPath = "./indexes.idx"
+
+func main() {
+	kv := kv.NewKV(dbPath, indexPath)
+	defer kv.Close()
+
+	// kv.Set("first_name", "Ivan")
+	// kv.Set("last_name", "Litvinenko")
+	// kv.Set("age", "45")
+	// kv.Set("salary", "54000")
+
+	// kv.Set("TEST_KEY", "TEST_VALUE")
+
+	// kv.Set("id", "777")
+	// kv.Set("email", "litvinenko@gmail.com")
+	// kv.Set("status", "ACTIVE")
+
+	// kv.Set("first_name", "Poll")
+	// kv.Set("last_name", "Andersson")
+	// kv.Flush()
+
+	fmt.Println("---", kv.Indexes)
+	fmt.Println("!!!", kv.MemTable)
+	kv.MemTable = make(map[string]string)
+	fmt.Println("!!!", kv.MemTable)
+
+	val, ok := kv.Get("last_name")
+	fmt.Printf("%v :::: %s\n", ok, val)
+
+	val, ok = kv.Get("first_name")
+	fmt.Printf("%v :::: %s\n", ok, val)
+
+	val, ok = kv.Get("salary")
+	fmt.Printf("%v :::: %s\n", ok, val)
+
+	val, ok = kv.Get("email")
+	fmt.Printf("%v :::: %s\n", ok, val)
+
+	val, ok = kv.Get("status")
+	fmt.Printf("%v :::: %s\n", ok, val)
+
+	val, ok = kv.Get("id")
+	fmt.Printf("%v :::: %s\n", ok, val)
+}
