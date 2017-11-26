28f53819c412ba4a097985586f6931e2c23f9a1a
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-11-26 11:49:01 +0100

    Benchmark improvement

--- kv/benchmark_test.go
+++ kv/benchmark_test.go
@@ -10,67 +10,106 @@ import (
 	"time"
 )
 
-func generateData(dir string, blockSize, numberOfKeys int) *KV {
-	dbPath := filepath.Join(dir, "data.db")
-	indexPath := filepath.Join(dir, "indexes.idx")
+func generateData(dbPath, indexPath string, blockSize, numberOfKeys int) {
+	// defer TimeTrack(time.Now(), "generateData")
 
 	store := NewKV(dbPath, indexPath, 100000)
 
 	for i := 0; i < numberOfKeys; i++ {
 		store.Set(fmt.Sprintf("key_%d", i), fmt.Sprintf("value_%d", i))
 	}
-	store.Flush()
-
-	store = NewKV(dbPath, indexPath, uint32(blockSize))
-
-	return store
+	store.Close()
 }
 
 func benchmarkGet(blockSize, numberOfKeys int, b *testing.B) {
+	// defer TimeTrack(time.Now(), "benchmarkGet")
+	b.StopTimer()
+
 	tmpDir, _ := ioutil.TempDir("", "benchmarkStore")
 	defer os.RemoveAll(tmpDir)
+	dbPath := filepath.Join(tmpDir, "data.db")
+	indexPath := filepath.Join(tmpDir, "indexes.idx")
 
-	store := generateData(tmpDir, blockSize, numberOfKeys)
+	generateData(dbPath, indexPath, blockSize, numberOfKeys)
+	store := NewKV(dbPath, indexPath, uint32(blockSize))
+	defer store.Close()
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+
 		index := r.Int31n(int32(numberOfKeys))
 		key := fmt.Sprintf("key_%d", index)
 		value, _ := store.Get(key)
+
 		if value != fmt.Sprintf("value_%d", index) {
 			fmt.Printf("Values mismatch `%s` expexted `%s`\n", value, fmt.Sprintf("value_%d", index))
 		}
+
+		b.SetBytes(int64(len([]byte(key))))
+		b.SetBytes(int64(len([]byte(value))))
+
+		b.StopTimer()
 	}
 }
 
 func benchmarkSet(blockSize, numberOfKeys int, b *testing.B) {
+	// defer TimeTrack(time.Now(), "benchmarkGet")
+	b.StopTimer()
+
 	tmpDir, _ := ioutil.TempDir("", "benchmarkStore")
 	defer os.RemoveAll(tmpDir)
+	dbPath := filepath.Join(tmpDir, "data.db")
+	indexPath := filepath.Join(tmpDir, "indexes.idx")
 
-	store := generateData(tmpDir, blockSize, numberOfKeys)
+	generateData(dbPath, indexPath, blockSize, numberOfKeys)
+	store := NewKV(dbPath, indexPath, uint32(blockSize))
+	defer store.Close()
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+
 		key := fmt.Sprintf("key_%d", i)
-		store.Set(key, fmt.Sprintf("value_%d", i))
+		value := fmt.Sprintf("value_%d", i)
+		store.Set(key, value)
+
+		b.SetBytes(int64(len([]byte(key))))
+		b.SetBytes(int64(len([]byte(value))))
+
+		b.StopTimer()
 	}
 }
 
 func benchmarkDelete(blockSize, numberOfKeys int, b *testing.B) {
+	// defer TimeTrack(time.Now(), "benchmarkDelete")
+	b.StopTimer()
+
 	tmpDir, _ := ioutil.TempDir("", "benchmarkStore")
 	defer os.RemoveAll(tmpDir)
+	dbPath := filepath.Join(tmpDir, "data.db")
+	indexPath := filepath.Join(tmpDir, "indexes.idx")
 
-	store := generateData(tmpDir, blockSize, numberOfKeys)
+	generateData(dbPath, indexPath, blockSize, numberOfKeys)
+	store := NewKV(dbPath, indexPath, uint32(blockSize))
+	defer store.Close()
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+
 		key := fmt.Sprintf("key_%d", i)
 		store.Delete(key)
+
+		b.SetBytes(int64(len([]byte(key))))
+		b.SetBytes(int64(len([]byte("__KVGO_TOMBSTONE__"))))
+
+		b.StopTimer()
 	}
 }
 
