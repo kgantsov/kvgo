ce5b2a0ab9eec1a6082c73e22774080d95a1b184
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-12-02 21:52:38 +0100

    Add Set/Get value parrallel test

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -62,3 +62,95 @@ func TestBasic(t *testing.T) {
 		assetEqual(t, fmt.Sprintf("key_%d", i), false, ok)
 	}
 }
+
+func TestBasicParallel(t *testing.T) {
+	t.Parallel()
+	tmpDir, _ := ioutil.TempDir("", "benchmarkStore")
+	defer os.RemoveAll(tmpDir)
+
+	dbPath := filepath.Join(".", "data.db")
+	indexPath := filepath.Join(".", "indexes.idx")
+
+	store := NewKV(dbPath, indexPath, 500)
+	N := 10000
+
+	t.Run("SET1", func(t *testing.T) {
+		t.Parallel()
+		for i := 0; i < N; i++ {
+			key := fmt.Sprintf("key_%d", i)
+			store.Set(key, fmt.Sprintf("value_%d", i))
+
+			expextedValue1 := fmt.Sprintf("value_%d", i)
+			expextedValue2 := fmt.Sprintf("value_%d", i*2)
+			expextedValue3 := fmt.Sprintf("value_%d", i*3)
+			value, _ := store.Get(key)
+
+			if (value != expextedValue1) && (value != expextedValue2) && (value != expextedValue3) {
+				t.Errorf(
+					"Expected `%v` or `%v` or `%v`. Got `%v`\n",
+					expextedValue1,
+					expextedValue2,
+					expextedValue3,
+					value,
+				)
+			}
+			// assetEqual(t, key, expextedValue, value)
+		}
+	})
+	t.Run("SET2", func(t *testing.T) {
+		t.Parallel()
+		for i := 0; i < N; i++ {
+			key := fmt.Sprintf("key_%d", i)
+			store.Set(key, fmt.Sprintf("value_%d", i*2))
+
+			expextedValue1 := fmt.Sprintf("value_%d", i)
+			expextedValue2 := fmt.Sprintf("value_%d", i*2)
+			expextedValue3 := fmt.Sprintf("value_%d", i*3)
+			value, _ := store.Get(key)
+
+			if (value != expextedValue1) && (value != expextedValue2) && (value != expextedValue3) {
+				t.Errorf(
+					"Expected `%v` or `%v` or `%v`. Got `%v`\n",
+					expextedValue1,
+					expextedValue2,
+					expextedValue3,
+					value,
+				)
+			}
+			// assetEqual(t, key, expextedValue, value)
+		}
+	})
+	t.Run("SET3", func(t *testing.T) {
+		t.Parallel()
+		for i := 0; i < N; i++ {
+			key := fmt.Sprintf("key_%d", i)
+			store.Set(key, fmt.Sprintf("value_%d", i*3))
+
+			expextedValue1 := fmt.Sprintf("value_%d", i)
+			expextedValue2 := fmt.Sprintf("value_%d", i*2)
+			expextedValue3 := fmt.Sprintf("value_%d", i*3)
+			value, _ := store.Get(key)
+
+			if (value != expextedValue1) && (value != expextedValue2) && (value != expextedValue3) {
+				t.Errorf(
+					"Expected `%v` or `%v` or `%v`. Got `%v`\n",
+					expextedValue1,
+					expextedValue2,
+					expextedValue3,
+					value,
+				)
+			}
+			// assetEqual(t, key, expextedValue, value)
+		}
+	})
+
+	// t.Run("GET", func(t *testing.T) {
+	// 	t.Parallel()
+	// 	for i := 0; i < N; i++ {
+	// 		expextedValue := fmt.Sprintf("value_%d", i)
+	// 		value, _ := store.Get(fmt.Sprintf("key_%d", i))
+	// 		assetEqual(t, fmt.Sprintf("key_%d", i), expextedValue, value)
+	// 	}
+	// 	store.Flush()
+	// })
+}
