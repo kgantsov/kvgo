80696ddd0c3f5a37de63ed253726ce7adf75b055
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-12-06 22:08:45 +0100

    Change example usage of storage in main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/kgantsov/kvgo/kv"
 )
@@ -13,43 +14,44 @@ func main() {
 	kv := kv.NewKV(dbPath, indexPath, 4)
 	defer kv.Close()
 
-	// kv.Set("first_name", "Ivan")
-	// kv.Set("last_name", "Litvinenko")
-	// kv.Set("age", "45")
-	// kv.Set("salary", "54000")
+	kv.Set("first_name", "Ivan")
+	kv.Set("last_name", "Litvinenko")
+	kv.Set("age", "45")
+	kv.Set("salary", "54000")
 
-	// kv.Set("TEST_KEY", "TEST_VALUE")
+	kv.Set("TEST_KEY", "TEST_VALUE")
 
-	// kv.Set("id", "777")
-	// kv.Set("email", "litvinenko@gmail.com")
-	// kv.Set("status", "ACTIVE")
+	kv.Set("id", "777")
+	kv.Set("email", "litvinenko@gmail.com")
+	kv.Set("status", "ACTIVE")
 
-	// kv.Set("last_name", "Andersson")
-	// kv.Flush()
-	// kv.Delete("first_name")
+	kv.Set("last_name", "Andersson")
+	kv.Flush()
+	kv.Delete("first_name")
 
-	// kv.Set("OPL", "KKKKo")
+	kv.Set("OPL", "KKKKo")
 
-	fmt.Println("---", len(kv.Indexes), kv.Indexes)
-	fmt.Println("!!!", kv.MemTable)
-	// kv.MemTable = make(map[string]string)
-	// fmt.Println("!!!", kv.MemTable)
-
+	start := time.Now()
 	val, ok := kv.Get("last_name")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 
+	start = time.Now()
 	val, ok = kv.Get("first_name")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 
+	start = time.Now()
 	val, ok = kv.Get("salary")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 
+	start = time.Now()
 	val, ok = kv.Get("email")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 
+	start = time.Now()
 	val, ok = kv.Get("status")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 
+	start = time.Now()
 	val, ok = kv.Get("id")
-	fmt.Printf("%v :::: %s\n", ok, val)
+	fmt.Printf("%v :::: %s TOOK: %s\n", ok, val, time.Since(start))
 }
