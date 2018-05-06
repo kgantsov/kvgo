b74d224cd03b073616488f905206f7e3f2590c74
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-05-06 19:04:25 +0200

    Fix redis clent

--- cmd/kvgod/redis_client_example/main.go
+++ cmd/kvgod/redis_client_example/main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/go-redis/redis"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Wrong number of arguments")
+	}
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:56379",
 		Password: "",
 		DB:       0,
 	})
 
-	err := client.Set("key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+	command := os.Args[1]
 
-	val, err := client.Get("key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	val2, err := client.Get("key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-
-	client.Del("key").Result()
-
-	val, err = client.Get("key").Result()
-	if err == redis.Nil {
-		fmt.Println("key does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key", val2)
+	switch command {
+	case "get":
+		val, err := client.Get(os.Args[2]).Result()
+		if err == redis.Nil {
+			fmt.Println(os.Args[2], "does not exist")
+		} else if err != nil {
+			panic(err)
+		} else {
+			fmt.Println(os.Args[2], "=", val)
+		}
+	case "set":
+		fmt.Println("set", os.Args[2], "to", os.Args[3])
+		err := client.Set(os.Args[2], os.Args[3], 0).Err()
+		if err != nil {
+			panic(err)
+		}
+	case "del":
+		fmt.Println("del", os.Args[2])
+		_, err := client.Del(os.Args[2]).Result()
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
 	}
 	client.Close()
 }
